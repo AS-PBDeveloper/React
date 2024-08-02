de62414944ebcd7505c795d9f8df5be3d64e93f3
Angkita Paul <168302404+AS-PBDeveloper@users.noreply.github.com>
2024-08-02 19:28:38 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+          This is an about component
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eligendi repellat, ipsa sequi eum quidem quia ab minus quas reiciendis totam nostrum cum sapiente. Aut.</p>
+    </div>
+  )
+}
+
+
